4b9bac3d372f422ef9e0f72841bcaa629f5c207e
trunn2004 <trung.nq.64cntt@ntu.edu.vn>
2025-03-11 00:51:47 +0700

    update

--- Ex5_AddSubMulDiv_Var/app/src/main/java/quctrun/trunn2004/ex5_addsubmuldiv_var/MainActivity.java
+++ Ex5_AddSubMulDiv_Var/app/src/main/java/quctrun/trunn2004/ex5_addsubmuldiv_var/MainActivity.java
@@ -1,6 +1,9 @@
 package quctrun.trunn2004.ex5_addsubmuldiv_var;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    //khai bao cac doi tuong 
+    //khai bao cac doi tuong ung voi cac dieu khien(view) can thao tac
+    EditText edtSoA,edtSoB;
+    Button btnCong,btnTru,btnNhan,btnChia;
+    TextView tvKetQua;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,18 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+        TimView();
     }
+    void TimView(){
+        edtSoA = (EditText) findViewById(R.id.editTextText);
+        edtSoB = (EditText) findViewById(R.id.editTextText2);
+        btnCong =(Button) findViewById(R.id.button);
+        btnTru =(Button) findViewById(R.id.button2);
+        btnNhan =(Button) findViewById(R.id.button3);
+        btnChia =(Button) findViewById(R.id.button4);
+    }
+
+
 }
