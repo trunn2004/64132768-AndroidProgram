3b943f4391838811aff718347b36e3210c2ebf3f
trunn2004 <trung.nq.64cntt@ntu.edu.vn>
2025-03-20 21:09:08 +0700

    bai tap lam them tren lop

---
+++ LT_Math1/app/src/main/java/quctrun/trunn2004/lt_math/MainActivity.java
@@ -0,0 +1,35 @@
+package quctrun.trunn2004.lt_math;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    CardView cv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        cv1 = findViewById(R.id.card1);
+        cv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMH2 = new Intent(MainActivity.this, AndroidLarge2.class);
+                startActivity(intentMH2);
+            }
+        });
+    }
+}
