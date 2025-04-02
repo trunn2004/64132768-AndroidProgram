e6d8114cdc304958ec04a7cb9e4f34925823182f
trunn2004 <trung.nq.64cntt@ntu.edu.vn>
2025-04-02 22:21:38 +0700

    RycycleView

--- nhatruong_thigk/android/app/src/main/kotlin/com/example/nhatruong_thigk/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.nhatruong_thigk
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- vonhattruong_thigk/android/app/src/main/kotlin/com/example/vonhattruong_thigk/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.vonhattruong_thigk
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
