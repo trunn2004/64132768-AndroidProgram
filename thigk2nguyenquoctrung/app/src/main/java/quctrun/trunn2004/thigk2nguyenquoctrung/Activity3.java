c6ae8b1283182b4c6ba82cf0187ccfcfc9b98d9a
trunn2004 <trung.nq.64cntt@ntu.edu.vn>
2025-04-04 14:34:23 +0700

    thi

--- thigk2nguyenquoctrung/app/src/main/java/quctrun/trunn2004/thigk2nguyenquoctrung/Activity3.java
+++ thigk2nguyenquoctrung/app/src/main/java/quctrun/trunn2004/thigk2nguyenquoctrung/Activity3.java
@@ -1,6 +1,12 @@
 package quctrun.trunn2004.thigk2nguyenquoctrung;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,40 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Activity3 extends AppCompatActivity {
+import java.util.ArrayList;
 
+import quctrun.trunn2004.thigk2nguyenquoctrung.ItemActiviti3;
+import quctrun.trunn2004.thigk2nguyenquoctrung.R;
+
+public class Activity3 extends AppCompatActivity {
+    ListView lv;
+    ArrayList<String> ds;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_3);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        ds = new ArrayList<String>();
+        ds.add("Kỹ thuật lập trình");
+        ds.add("Kỹ thuật đồ họa");
+        ds.add("Thiết kế Wed");
+        ds.add("Phát triển ứng dụng Wed");
+        ds.add("Hệ điều hành");
+        ds.add("Lập trình thiết bị di động");
+        ArrayAdapter<String> adapterNNLT;
+        adapterNNLT = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, ds);
+        lv.setAdapter(adapterNNLT);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //code xu ly o day
+                String giaTriDuocChon = ds.get(position);
+                //Toast.makeText(ActivityCau2.this, giaTriDuocChon, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Activity3.this, ItemActiviti3.class);
+                intent.putExtra("selectedItem", giaTriDuocChon);
+                startActivity(intent);
+            }
         });
+
     }
 }
--- thigk2nguyenquoctrung/app/src/main/java/quctrun/trunn2004/thigk2nguyenquoctrung/MainActivity.java
+++ thigk2nguyenquoctrung/app/src/main/java/quctrun/trunn2004/thigk2nguyenquoctrung/MainActivity.java
@@ -1,6 +1,9 @@
 package quctrun.trunn2004.thigk2nguyenquoctrung;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv1,tv2,tv3,tv4,tv5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,41 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TimDieuKhien();
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IntentMH1 = new Intent(MainActivity.this, Activity2.class);
+                startActivity(IntentMH1);
+            }
+        });
+        tv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IntentMH2 = new Intent(MainActivity.this, Activity3.class);
+                startActivity(IntentMH2);
+            }
+        });
+        tv3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IntentMH3 = new Intent(MainActivity.this, Activity4.class);
+                startActivity(IntentMH3);
+            }
+        });
+        tv4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IntentMH4 = new Intent(MainActivity.this, Activity5.class);
+                startActivity(IntentMH4);
+            }
+        });
+    }
+    public void TimDieuKhien(){
+        tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
+        tv3 = findViewById(R.id.tv3);
+        tv4 = findViewById(R.id.tv4);
+        tv5 = findViewById(R.id.tv5);
     }
 }
