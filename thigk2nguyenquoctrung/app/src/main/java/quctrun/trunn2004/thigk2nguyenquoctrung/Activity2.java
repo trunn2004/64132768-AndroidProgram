451b13481c9f0eeef36864949f575477ab3d57b8
trunn2004 <trung.nq.64cntt@ntu.edu.vn>
2025-04-04 14:47:43 +0700

    thi

--- thigk2nguyenquoctrung/app/src/main/java/quctrun/trunn2004/thigk2nguyenquoctrung/Activity2.java
+++ thigk2nguyenquoctrung/app/src/main/java/quctrun/trunn2004/thigk2nguyenquoctrung/Activity2.java
@@ -1,6 +1,9 @@
 package quctrun.trunn2004.thigk2nguyenquoctrung;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Activity2 extends AppCompatActivity {
-
+    EditText edtsoA,edtsob;
+    Button btntong;
+    EditText edtkq;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,22 @@ public class Activity2 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        EditText edtA = findViewById(R.id.edtgk);
+        EditText edtB = findViewById(R.id.edtck);
+        EditText edtKQ = findViewById(R.id.edtkq);
+        Button btntong = findViewById(R.id.btnTB);
+        btntong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strA = edtA.getText().toString();
+                String strB = edtB.getText().toString();
+                float soA = Float.parseFloat(strA);
+                float soB = Float.parseFloat(strB);
+                float tong = (soA + soB)/2 ;
+                String strTong = String.valueOf(tong);
+                edtKQ.setText(strTong);
+            }
         });
     }
 }
--- thigk2nguyenquoctrung/app/src/main/java/quctrun/trunn2004/thigk2nguyenquoctrung/Activity3.java
+++ thigk2nguyenquoctrung/app/src/main/java/quctrun/trunn2004/thigk2nguyenquoctrung/Activity3.java
@@ -22,32 +22,36 @@ import quctrun.trunn2004.thigk2nguyenquoctrung.R;
 public class Activity3 extends AppCompatActivity {
     ListView lv;
     ArrayList<String> ds;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_3);
-        ds = new ArrayList<String>();
+        
+        lv = findViewById(R.id.listView);
+
+        ds = new ArrayList<>();
         ds.add("Kỹ thuật lập trình");
         ds.add("Kỹ thuật đồ họa");
-        ds.add("Thiết kế Wed");
-        ds.add("Phát triển ứng dụng Wed");
+        ds.add("Thiết kế Web");
+        ds.add("Phát triển ứng dụng Web");
         ds.add("Hệ điều hành");
         ds.add("Lập trình thiết bị di động");
-        ArrayAdapter<String> adapterNNLT;
-        adapterNNLT = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, ds);
+
+        ArrayAdapter<String> adapterNNLT = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ds);
         lv.setAdapter(adapterNNLT);
+
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //code xu ly o day
                 String giaTriDuocChon = ds.get(position);
-                //Toast.makeText(ActivityCau2.this, giaTriDuocChon, Toast.LENGTH_SHORT).show();
+                Toast.makeText(Activity3.this, giaTriDuocChon, Toast.LENGTH_SHORT).show();
+
                 Intent intent = new Intent(Activity3.this, ItemActiviti3.class);
                 intent.putExtra("selectedItem", giaTriDuocChon);
                 startActivity(intent);
             }
         });
-
     }
 }
